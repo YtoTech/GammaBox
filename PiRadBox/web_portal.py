ef7c440357b2027364835f4699893e754b99a015
Yoan Tournade <yoan@ytotech.com>
2015-12-15 02:21:31 +0100

    Ok we can play sound. I mean geiger click sounds. Let's link them to our counter through WebSockets

--- PiRadBox/web_portal.py
+++ PiRadBox/web_portal.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
