8f36c3540cda4ab0fbbf1a1114045ad349bea1d2
Yoan Tournade <yoan@ytotech.com>
2016-03-03 02:31:43 +0000

    Ok I thing I understood greenthread. Clearly I'm not happy how we're transfering the radiation signal from the GPIO interrupt thread to the SocketIO thread, using polling in a greenthread of the latter thread

--- PiRadBox/stream.py
+++ PiRadBox/stream.py
@@ -2,11 +2,18 @@ from flask_socketio import SocketIO, emit
 from PiPocketGeiger import RadiationWatch
 from web_portal import app
 import time
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+import eventlet
+eventlet.monkey_patch()
 
 radiationWatch = RadiationWatch(24, 23).setup()
 # We need to close properly this resource at the appplication tear down.
 
-socketio = SocketIO(app, async_mode='threading')
+socketio = SocketIO(app)
+q = queue.Queue()
 
 @socketio.on('connect')
 def onConnect():
@@ -19,10 +26,26 @@ def onConnect():
     # emit('historical', data, json=True)
 
 def onRadiation():
-    # TODO Get back to our main eventlet thread.
+    # TODO Get back to our main eventlet thread: how to to that?
+    # Use a Queue, then create a greenlet that poll to the queue?
+    # Or use a Python socketio client to communicate with this server.
     print("Ray hit")
-    socketio.emit('ray', 'Hit!')
+    q.put('Hit!')
+    # socketio.emit('ray', 'Hit!')
     # TODO Send current readings.
     # socketio.emit('ray', readings, json=True)
 
+def listenToQueue():
+    while 1:
+        # This is fucking ugly! We're polling as hell.
+        try:
+            data = q.get(block=False)
+            print(data)
+            socketio.emit('ray', data)
+        except queue.Empty:
+            eventlet.sleep(0.1)
+
+eventlet.spawn(listenToQueue)
+
 radiationWatch.registerRadiationCallback(onRadiation)
+
