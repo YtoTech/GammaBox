ebffc3455a4a9865e919d66c9f94fffafba917de
Yoan Tournade <yoan@ytotech.com>
2016-03-03 00:02:12 +0000

    Ok we've got the SocketIO thing. But know we have to make work the PiPocketGeiger lib with the Flask app and gunicorn

--- PiRadBox/stream.py
+++ PiRadBox/stream.py
@@ -2,18 +2,20 @@ from flask_socketio import SocketIO, emit
 from web_portal import app
 import time
 
-# Please read the doc: https://flask-socketio.readthedocs.org/en/latest/
-
 socketio = SocketIO(app)
 
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
-
-@socketio.on('connect', namespace='/chat')
-def test_connect():
-    emit('my response', {'data': 'Connected'})
+@socketio.on('connect')
+def onConnect():
+    print('Client connected')
+    emit('readings', {
+        'cpm': '-',
+        'uSvh': '-'
+        }, json=True)
+    # TODO Send Historical data.
+    # emit('historical', data, json=True)
 
 def onRadiation():
     print("Ray hit")
-    socketio.send('Ray', namespace='/radiation/rays')
+    socketio.emit('ray', 'Hit!')
+    # TODO Send current readings.
+    # socketio.emit('ray', readings, json=True)
