d55ca9d2617945a796eda8860c591057d9eb6f89
Yoan Tournade <yoan@ytotech.com>
2016-04-20 05:34:47 +0200

    Create the settings form using React. Still the interaction to code, and then the persistence server side.

--- PiRadBox/web_portal.py
+++ PiRadBox/web_portal.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/settings")
+def settings():
+    return render_template('settings.html')
