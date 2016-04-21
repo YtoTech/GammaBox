76ee98b8ee0a00e7d184b7d49118c1c72711d57b
Yoan Tournade <yoan@ytotech.com>
2016-04-21 04:22:54 +0200

    Now need a way to persist the settings state in the server and access it from the api methods.

--- PiRadBox/web_portal.py
+++ PiRadBox/web_portal.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+# TODO Persist and load settings to file.
+# TODO Create an object accessible from the app methods.
+radboxSettings = None
 
 @app.route("/")
 def index():
@@ -9,3 +12,16 @@ def index():
 @app.route("/settings")
 def settings():
     return render_template('settings.html')
+
+# API.
+
+@app.route("/api/settings", methods=['GET'])
+def api_settings_get():
+    print(radboxSettings)
+    return jsonify(settings)
+
+@app.route("/api/settings", methods=['POST'])
+def api_settings_post():
+    print(request.get_json())
+    radboxSettings = request.get_json()
+    return '', 204
